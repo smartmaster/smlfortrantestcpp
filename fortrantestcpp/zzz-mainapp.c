4917c60c9a7c797819102a6249f4128c9bb071b2
smart.master <smart.master@gmail.com>
2022-02-27 12:23:06 +0800

    add new main.c for test program
    Signed-off-by: smart.master <smart.master@gmail.com>

---
+++ fortrantestcpp/zzz-mainapp.c
@@ -0,0 +1,32 @@
+#include "../../fortrantest/f2c.h"
+
+#include "../../fortrantest/oldf77.P"
+#include "../../fortrantest/oldf7701.P"
+#include "../../fortrantest/oldf7702.P"
+#include "../../fortrantest/oldf7703.P"
+#include "../../fortrantest/oldf7704.P"
+
+int main(int atgc, char** argv)
+{
+
+	integer ii1 = 100;
+	char cc1[] = "helloooo";
+	longint ii2 = 200;
+	char cc2[] = "worldddd";
+	integer arr[5];
+	integer arrlen = 5;
+	ftnlen cc1_len = sizeof(cc1) - 1;
+	ftnlen cc2_len = sizeof(cc2) - 1;
+
+	const int loop = 3;
+	for (int ii = 0; ii < loop; ++ii)
+	{
+		oldf77_();
+		oldf7701_();
+		oldf7702_();
+		oldf7703_();
+		oldf7704_(&ii1, cc1, &ii2, cc2, arr, &arrlen, cc1_len, cc2_len);
+	}
+
+	return 0;
+}
